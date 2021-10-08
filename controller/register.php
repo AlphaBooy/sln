02fa101ef486a7df56eca9404827fdb5d0ac4c2e
Clément Maurin <clementmaurin.off@gmail.com>
2021-10-08 16:50:32 +0200

    Register page creation

--- controller/register.php
+++ controller/register.php
@@ -4,6 +4,6 @@ require_once "../utils/header.php";
 // Include a navigation bar that allow the user to navigate through the site
 require_once "../utils/navbar.php";
 // Include the HTML + CSS part of the HOME page
-require_once "../view/login/loginView.html";
+require_once "../view/register/registerView.html";
 // Include the footer part of the HTML document (closing brackets mainly)
 require_once "../utils/footer.php";
