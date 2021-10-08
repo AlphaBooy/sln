d7c9c490dc927c5b5e9c770557ee47e19c1767d3
Clément Maurin <clementmaurin.off@gmail.com>
2021-10-08 14:19:26 +0200

    Improving home page

--- utils/header.php
+++ utils/header.php
@@ -2,5 +2,6 @@
 <head>
     <title>SLN</title>
     <link href="../utils/styles/globalStyle.css" rel="stylesheet">
+    <link href="../public/fontawesome/css/all.min.css" rel="stylesheet">
 </head>
 <body>
--- utils/navbar.php
+++ utils/navbar.php
@@ -2,8 +2,8 @@
     <span class="logo"><a href="../index.php?action=index">SLN</a></span>
     <nav>
         <ul>
-            <li><a href="../index.php?action=register">&nbsp; Register</a></li>
-            <li><a href="../index.php?action=login">&nbsp; Login</a></li>
+            <li><a href="../index.php?action=register" class="btn-navbar"><i class="fas fa-user-plus"></i> &nbsp; Register</a></li>
+            <li><a href="../index.php?action=login" class="btn-navbar"><i class="fas fa-sign-in-alt"></i> &nbsp; Login</a></li>
         </ul>
     </nav>
 </div>
