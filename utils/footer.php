adaf44f78447e134d841a9df13ca57b95bcc3700
Clément Maurin <clementmaurin.off@gmail.com>
2021-10-10 09:01:53 +0200

    CSS code improvement and cleaned up

--- utils/footer.php
+++ utils/footer.php
@@ -1,2 +1,8 @@
-</body>
+        <div class="footer w-100 text-centered">
+            <div>
+                <span class="text-primary">This site has been developed for a school project at <a href="#">3IL ingénieur (Rodez)</a></span>
+            </div>
+            <span class="text-primary mx-auto">Sympas les NFT&nbsp;<i class="far fa-copyright"></i>&nbsp;2021</span>
+        </div>
+    </body>
 </html>
--- utils/header.php
+++ utils/header.php
@@ -1,7 +1,8 @@
 <html>
 <head>
     <title>SLN</title>
-    <link href="../utils/styles/globalStyle.css" rel="stylesheet">
     <link href="../public/fontawesome/css/all.min.css" rel="stylesheet">
+    <link href="../utils/styles/globalStyle.css" rel="stylesheet">
+    <link rel="shortcut icon" type="image/x-icon" href="../public/resources/image/logo.png" />
 </head>
 <body>
