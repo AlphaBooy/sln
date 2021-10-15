c81dee717042c3f0e26138683f658f7beb5168e0
Clément Maurin <clementmaurin.off@gmail.com>
2021-10-15 11:50:27 +0200

    Style changes on home and login

--- utils/footer.php
+++ utils/footer.php
@@ -1,6 +1,6 @@
         <div class="footer w-100 text-centered">
             <div>
-                <span class="text-primary">This site has been developed for a school project at <a href="#">3IL ingénieur (Rodez)</a></span>
+                <span class="text-primary">This site has been developed for a school project at <a href="#">3IL ingénieur (Rodez) <sup><i class="fas fa-info-circle"></i></sup></a></span>
             </div>
             <span class="text-primary mx-auto">Sympas les NFT&nbsp;<i class="far fa-copyright"></i>&nbsp;2021</span>
         </div>
